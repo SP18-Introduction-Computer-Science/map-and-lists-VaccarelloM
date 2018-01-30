83a9ca997f0c911d8fff83b1fbac29ab63e64aee
Matthew Vaccarello <35546706+VaccarelloM@users.noreply.github.com>
2018-01-30 13:06:43 -0600

    Add files via upload

---
+++ Maps and Lists.py
@@ -0,0 +1,17 @@
+a=[1,2,3,4]
+print(a)
+
+def multiply(x):
+    return (x*x)
+def add(x):
+    return (x+x)
+
+funcs = [multiply, add]
+for i in range(5):
+    value = list(map(lambda x: x(i), funcs))
+    print(value)
+
+
+
+
+
